f5b7fb7c26101e6d0cc19d9430b8ac6657d3f456
Jukka Ahlgren <jukka.ahlgren@helsinki.fi>
2021-01-12 12:53:44 +0200

    Add eslint

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
-app.use((req, res, next) => {
+app.use((req, res) => {
   res.send('Welcome to Express');
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+  console.log(`Server running on port ${port}`);
 });
